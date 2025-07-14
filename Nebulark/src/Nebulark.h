28145f12139a48e7e2c90f98b941de18817df8fa
RicoViviers <ricoleonardviviers@gmail.com>
2025-07-14 12:54:11 +0200

    Add logging system for console support

--- Nebulark/src/Nebulark.h
+++ Nebulark/src/Nebulark.h
@@ -3,7 +3,7 @@
 
 // For use by Nebulark applications
 #include "Nebulark/Application.h"
-
+#include "Nebulark/Log.h"
 // ---Entry Point-------------------
 #include "Nebulark/EntryPoint.h"
 // ---------------------------------
